ffcfad178ac35c2519bbcf7e82ca1a146a11358f
willymateo <wjmateo@espol.edu.ec>
2021-12-02 14:44:05 -0500

    Estructura de busqueda de imagen

--- js/noticias/noticias.js
+++ js/noticias/noticias.js
@@ -17,10 +17,15 @@ function load_notices(data) {
 		let num_coment = i.coments;
 
 		let plantilla=`
-		<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="col-md-4 col-sm-6 col-xs-12 notice">
 			<div class="probootstrap-hentry mb30">
-				<p><a href="#"><img src="${srcImage}" alt="${altImage}" class="img-responsive"></a></p>
-				<h3 class="mt0"><a href="#" class="hover-reverse">${notice_title}</a></h3>
+				<p>
+					<a href="#"><img src="${srcImage}" alt="${altImage}" class="img-responsive">
+					</a>
+				</p>
+				<h3 class="mt0">
+					<a href="#" class="hover-reverse">${notice_title}</a>
+				</h3>
 				<p class="text-mute">${dateFinal} • <i class="icon-bubble"></i>${num_coment}</p>
 			</div>
 		</div>
@@ -47,63 +52,25 @@ const peticion = () => {
 		})
 		.catch(console.error);
 }
-peticion()
 
-//Corregir.
-const search_notices = (search_bar) => {
+const search_notices = () => {
+	let search_bar = document.querySelector('input[type="search"]');
 	let search_key = search_bar.value.toLowerCase();
-	let cards = document.getElementsByClassName("col");
+	let cards = document.getElementsByClassName("notice");
 
 	Array.from(cards).forEach(card => {
-		if (search_key === "") {
+		if (search_key === "")
 			card.classList.remove("d-none");
-		} else {
-			let title = card.querySelector(".card-body > h5.card-title");
-			let description = card.querySelector(".card-body > p.card-text");
-
-			let match_title = title.innerText.toLowerCase().includes(search_key);
-			let match_description = description.innerText.toLowerCase().includes(search_key);
-
-			if (!match_title && !match_description) {
+		else {
+			let notice_title = card.querySelector("a.hover-reverse");
+			let match_title = notice_title.innerText.toLowerCase().includes(search_key);
+			if (!match_title)
 				card.classList.add("d-none");
-			}
 		}
 	})
 }
 
-let search_bar = document.querySelector('input[type="search"]');
-search_bar.addEventListener("keydown", search_notices(search_bar));
-
-
-
-//Liz guia
-const buscador = () => {
-	let texto = document.querySelector('input[type="search"]')
-
-
-	texto.addEventListener('keyup', () => {
-		let texto = document.querySelector('input').value
-
-		let elementos = document.getElementsByClassName('col-md-4 col-sm-6 col-xs-12')
-		// console.log(elementos);
-		if(texto.length > 0){
-
-			for (let e of elementos){
-				//  console.log(e.innerText);
-				//  console.log(texto);
-				if(e.innerText.includes(texto)){
-					console.log("hola")
-					e.classList.add('d-inline')
-				} else {
-					e.classList.add('d-none')
-				}
-			}
-		} else {
-			for(let elemento of elementos){
-				elemento.classList.remove('d-none')
-				elemento.classList.remove('d-inline')
-			}
-		}
-	})
-}
+peticion();
+let search_button = document.querySelector('button');
+search_button.addEventListener("click", search_notices);
 
