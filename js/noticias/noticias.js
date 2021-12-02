e266340a14b6812a1472194dc85b7b631c513b34
willymateo <wjmateo@espol.edu.ec>
2021-12-02 10:58:27 -0500

    Refactorización de carga de json, script para correr browser syc

--- js/noticias/noticias.js
+++ js/noticias/noticias.js
@@ -1,124 +1,109 @@
+function load_notices(data) {
+	let c = 0;
+	let c2 = 0;
+	for( i of data){
+		c++;
+		c2++;
+		let fecha = i.fecha;
+		let d = new Date(fecha)
+		let anio = d.getFullYear();
+		let mes = d.getMonth();
+		let dia = d.getDate();
 
+		let srcImage = i.src;
+		let altImage = i.alt;
+		let notice_title = i.noticia;
+		let dateFinal =  ((mes+1)+"/"+dia+"/"+anio);
+		let num_coment = i.coments;
 
-let plantilla=`
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <div class="probootstrap-hentry mb30">
-        <p><a href="#"><img src="IMAGEN" alt="ALT" class="img-responsive"></a></p>
-        <h3 class="mt0"><a href="#" class="hover-reverse">NOTICIA</a></h3>
-        <p class="text-mute">FECHA • <i class="icon-bubble"></i>COMENTARIO</p>
-    </div>
-`
+		let plantilla=`
+		<div class="col-md-4 col-sm-6 col-xs-12">
+			<div class="probootstrap-hentry mb30">
+				<p><a href="#"><img src="${srcImage}" alt="${altImage}" class="img-responsive"></a></p>
+				<h3 class="mt0"><a href="#" class="hover-reverse">${notice_title}</a></h3>
+				<p class="text-mute">${dateFinal} • <i class="icon-bubble"></i>${num_coment}</p>
+			</div>
+		</div>
+		`
 
-const peticion = () => {
-    fetch("data/noticias.json")
-    .then(response => response.json())
-    .then(data => {
-        let c = 0;   
-        let c2 = 0; 
-        for( i of data){
-            c++;
-            c2++;
-            let section = document.getElementsByName("rownoticias");
-            
-            let fecha = i.fecha;
-            let d = new Date(fecha)
-            let anio = d.getFullYear();
-            let mes = d.getMonth();
-            let dia = d.getDate();
+		if(c == 3){
+			c = 1;
+			plantilla = `<div class="clearfix visible-sm-block"></div>` + plantilla;
+		}else if (c2 == 4){
+			c ++;
+			plantilla = `<div class="clearfix visible-lg-block visible-md-block"></div>` + plantilla;
+		}
 
-            let fechaf =  ((mes+1)+"/"+dia+"/"+anio);
-            let noticia = i.noticia;
-            let src = i.src;
-            let alt = i.alt;
-            let come = i.coments;
-            if(c == 3){
-                c = 1;
-                 plantilla = `
-                <div class="clearfix visible-sm-block"></div>
-
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="probootstrap-hentry mb30">
-                        <p><a href="#"><img src="IMAGEN" alt="ALT" class="img-responsive"></a></p>
-                        <h3 class="mt0"><a href="#" class="hover-reverse">NOTICIA</a></h3>
-                        <p class="text-mute">FECHA • <i class="icon-bubble"></i>COMENTARIO</p>
-                    </div>
-                 </div>
-                `
-
-            }else if (c2 == 4){
-                c ++;
-                
-                plantilla = `
-                <div class="clearfix visible-lg-block visible-md-block"></div>
-                
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="probootstrap-hentry mb30">
-                        <p><a href="#"><img src="IMAGEN" alt="ALT" class="img-responsive"></a></p>
-                        <h3 class="mt0"><a href="#" class="hover-reverse">NOTICIA</a></h3>
-                        <p class="text-mute">FECHA • <i class="icon-bubble"></i>COMENTARIO</p>
-                    </div>
-                </div>
-            `
-
-            }else{
-                plantilla=`
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="probootstrap-hentry mb30">
-                        <p><a href="#"><img src="IMAGEN" alt="ALT" class="img-responsive"></a></p>
-                        <h3 class="mt0"><a href="#" class="hover-reverse">NOTICIA</a></h3>
-                        <p class="text-mute">FECHA • <i class="icon-bubble"></i>COMENTARIO</p>
-                    </div>
-                `
-            }  
-            plantilla = plantilla.replace("FECHA", fechaf);
-            plantilla = plantilla.replace("NOTICIA", noticia);
-            plantilla = plantilla.replace("IMAGEN", src);
-            plantilla = plantilla.replace("ALT", alt);
-            plantilla = plantilla.replace("COMENTARIO", come);
-            // console.log(plantilla);
-            document.getElementById('cambio').innerHTML+= plantilla;
-            
-        }
-
-    })
-    .catch(console.error);
-
-
+		//Agregar noticia al feed.
+		document.getElementById('cambio').innerHTML+= plantilla;
+	}
 }
 
+const peticion = () => {
+	fetch("data/noticias.json")
+		.then(response => response.json())
+		.then(data => {
+			load_notices(data);
+		})
+		.catch(console.error);
+}
 peticion()
+
+//Corregir.
+const search_notices = (search_bar) => {
+	let search_key = search_bar.value.toLowerCase();
+	let cards = document.getElementsByClassName("col");
+
+	Array.from(cards).forEach(card => {
+		if (search_key === "") {
+			card.classList.remove("d-none");
+		} else {
+			let title = card.querySelector(".card-body > h5.card-title");
+			let description = card.querySelector(".card-body > p.card-text");
+
+			let match_title = title.innerText.toLowerCase().includes(search_key);
+			let match_description = description.innerText.toLowerCase().includes(search_key);
+
+			if (!match_title && !match_description) {
+				card.classList.add("d-none");
+			}
+		}
+	})
+}
+
+let search_bar = document.querySelector('input[type="search"]');
+search_bar.addEventListener("keydown", search_notices(search_bar));
+
+
+
+//Liz guia
 const buscador = () => {
-    let texto = document.querySelector('input')
-  
-  
-    texto.addEventListener('keyup', () => {
-      let texto = document.querySelector('input').value 
-  
-      let elementos = document.getElementsByClassName('col-md-4 col-sm-6 col-xs-12')
-      // console.log(elementos);
-      if(texto.length > 0){
-  
-        for (let e of elementos){
-            //  console.log(e.innerText);
-            //  console.log(texto);
-          if(e.innerText.includes(texto)){
-            console.log("hola")
-            e.classList.add('d-inline')
-          } else {
-            e.classList.add('d-none')
-          }      
-        }
-      } else {
-        for(let elemento of elementos){
-          elemento.classList.remove('d-none')
-          elemento.classList.remove('d-inline')
-        }
-      }
-  
-      
-    })
-  
-  
-  
-  }
-  buscador();
+	let texto = document.querySelector('input[type="search"]')
+
+
+	texto.addEventListener('keyup', () => {
+		let texto = document.querySelector('input').value
+
+		let elementos = document.getElementsByClassName('col-md-4 col-sm-6 col-xs-12')
+		// console.log(elementos);
+		if(texto.length > 0){
+
+			for (let e of elementos){
+				//  console.log(e.innerText);
+				//  console.log(texto);
+				if(e.innerText.includes(texto)){
+					console.log("hola")
+					e.classList.add('d-inline')
+				} else {
+					e.classList.add('d-none')
+				}
+			}
+		} else {
+			for(let elemento of elementos){
+				elemento.classList.remove('d-none')
+				elemento.classList.remove('d-inline')
+			}
+		}
+	})
+}
+
